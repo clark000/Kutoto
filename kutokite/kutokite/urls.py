1bc1a3998f38537f0bf686622bbc5207afe0830d
伊藤熊吉 <fuxinlei001@126.com>
2013-08-24 17:53:45 +0800

    start project clear

--- kutokite/kutokite/urls.py
+++ kutokite/kutokite/urls.py
@@ -4,12 +4,6 @@ from django.contrib import admin
 admin.autodiscover()
 
 urlpatterns = patterns('kutokite.views',
-    # Examples:
-    url(r'^$', 'home', name='home'),
-    # url(r'^blog/', include('blog.urls')),
-
-    url(r'^admin/', include(admin.site.urls)),
-    url(r'^oauth2/',include('oauth2.urls') )
 )
 urlpatterns += patterns('',
 )
--- kutokite/kutokite/views.py
+++
@@ -1,5 +0,0 @@
-#coding:utf-8
-from django.shortcuts import render,render_to_response
-from django.http import HttpResponse,Http404
-def home(reuqest):
-	return HttpResponse(u'Home page.<a href="/oauth2/login"><input type="button" value="第三方登录" class="btn" /></a>')
