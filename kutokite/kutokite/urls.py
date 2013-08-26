38b44cc043b23b52775235cfef100bb2a19ed4f9
伊藤熊吉 <fuxinlei001@126.com>
2013-08-26 08:59:47 +0800

    clear_urls.py

--- kutokite/kutokite/urls.py
+++ kutokite/kutokite/urls.py
@@ -3,7 +3,5 @@ from django.conf.urls import patterns, include, url
 from django.contrib import admin
 admin.autodiscover()
 
-urlpatterns = patterns('kutokite.views',
-)
-urlpatterns += patterns('',
+urlpatterns = patterns('',
 )
